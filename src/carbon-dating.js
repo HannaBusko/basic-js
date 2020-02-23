a3bfe4d3a0a767041e0d44c9004cbf0267e8afd0
HannaBusko <56487177+HannaBusko@users.noreply.github.com>
2020-02-23 19:00:40 +0300

    feat: add 2 tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,17 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
+const LOG2 = 0.693;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleAct) {
+
+  if (typeof (sampleAct) != "string")
+    return false;
+
+  let numSampleAct = parseFloat(sampleAct);
+
+  if (isNaN(numSampleAct) || numSampleAct <= 0 || numSampleAct > 15)
+    return false;
+
+  let ageStr = Math.ceil(Math.log(MODERN_ACTIVITY / numSampleAct) * HALF_LIFE_PERIOD /LOG2 );
+  return ageStr;
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,12 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  let numCats = 0;
+  let cat = "^^";
+  for (let row of backyard) {
+    for (let something of row) {
+      if (typeof (something) == "string" && something.includes(cat))
+        if (something.length == cat.length)
+          numCats++;
+    }
+  }
+  return numCats;
 };
