e3aa764a001dbb395aef3d66d02176980c1bc01c
HannaBusko <56487177+HannaBusko@users.noreply.github.com>
2020-02-24 14:22:14 +0300

    feat: add 4 task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,30 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+
+    if (typeof (str) != "string") {
+        str = String(str);
+    }
+    let separator = options.separator ? options.separator : "+";
+    let additionSeparator = options.additionSeparator ? options.additionSeparator : "|";
+    let addition = typeof(options.addition) != "undefined" ? String(options.addition) : "";
+    let repeatTimes = options.repeatTimes ? options.repeatTimes : 1;
+    let additionRepeatTimes = options.additionRepeatTimes ? options.additionRepeatTimes : 1;
+
+    let strAdd = "";
+    let newStr="";
+    if(additionRepeatTimes==1)
+      strAdd = addition;
+    else
+    {
+      for(let j=1; j<additionRepeatTimes;j++){
+        strAdd += additionSeparator+addition;
+      }
+      strAdd = addition+strAdd;
+    }
+    if (repeatTimes>1) {
+        for (let i = 1; i < repeatTimes; i++) {
+            newStr += separator + str + strAdd;
+        }
+    }
+
+    return str + strAdd + newStr; 
 };
-  
